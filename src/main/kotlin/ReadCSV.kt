2a5fce49ed252ff7c3a46f11b9adb0831d6d5e9c
vnaranjo <vnaranjo@bemen3.cat>
2024-12-19 18:01:51 +0100

    Modificació de la classe de lectura d'arxius. Fa servir la classe Carto.  Faltava l'import.  Ara ja escriu a fitxer, però heu de tenir en compte que no escriu en format csv.  Només escriu a fitxer.

--- src/main/kotlin/ReadCSV.kt
+++ src/main/kotlin/ReadCSV.kt
@@ -1,3 +1,6 @@
+import java.io.FileOutputStream
+import java.io.OutputStream
+
 fun OutputStream.writeCsv(cartons: List<Carto>) {
     val writer = bufferedWriter()
     writer.write(""""Fila 1", "Fila 2", "Fila 3"""")
@@ -9,9 +12,13 @@ fun OutputStream.writeCsv(cartons: List<Carto>) {
     writer.flush()
 }
 fun main() {
-    val fila1 = "3, 5, 1"
-    val fila2 = "5, 2, 2"
-    val fila3 = "5, 3, 2"
-    val cartons = Carto(fila1, fila2, fila3)
+    val fila1 = arrayListOf(3,5,1)
+    val fila2 = arrayListOf(5,2,2)
+    val fila3 = arrayListOf(3,9,1)
+    val cartons = arrayListOf (
+        Carto(fila1, fila2, fila3),
+        Carto(fila1, fila2, fila3)
+    )
+    FileOutputStream("filename.csv").apply { writeCsv(cartons) }
 }
-FileOutputStream("filename.csv").apply { writeCsv(cartons) }
+
