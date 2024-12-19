64fa66e5bca0f4ccf089f9e7d28df1f41bf893d4
vnaranjo <vnaranjo@bemen3.cat>
2024-12-19 18:07:27 +0100

    Noms de variables en minúscula!!! en llegir el codi es confonia la variable Carton, i semblava que es referia a la classe Carto, però en realitat era un array referenciat per una variable amb nom de Classe (amb majúscules)

--- src/main/kotlin/GenerateCard.kt
+++ src/main/kotlin/GenerateCard.kt
@@ -3,34 +3,34 @@ import kotlin.random.Random
 
 
 fun main(){
-    val Columns: Int = 5;
-    val Rows: Int = 3;
+    val columnes: Int = 5;
+    val rows: Int = 3;
 
-    var Carton = Array(Columns) { Array<Int>(Rows) { 0 } }
-    val UsedNums = mutableListOf(0)
+    var carton = Array(columnes) { Array<Int>(rows) { 0 } }
+    val usedNums = mutableListOf(0)
 
     var newnum:Int = 0;
-    for (i in 0..Rows-1){
-        for (j in 0..Columns-1){
-            while(UsedNums.contains(newnum)){
+    for (i in 0..rows-1){
+        for (j in 0..columnes-1){
+            while(usedNums.contains(newnum)){
                 newnum = Random.nextInt(0, 100)
             }
-            Carton[j][i]= newnum
-            UsedNums.add(newnum)
+            carton[j][i]= newnum
+            usedNums.add(newnum)
         }
     }
-    var ToCsv: String ="";
-    for (i in 0..Rows-1){
-        for (j in 0..Columns-1){
+    var toCsv: String ="";
+    for (i in 0..rows-1){
+        for (j in 0..columnes-1){
 
-            ToCsv = ToCsv + Carton[j][i].toString()
-            if(j != Columns-1){
-                ToCsv = ToCsv + ","
+            toCsv = toCsv + carton[j][i].toString()
+            if(j != columnes-1){
+                toCsv = toCsv + ","
             }
         }
-        ToCsv= ToCsv + "\n"
+        toCsv= toCsv + "\n"
     }
-    println(ToCsv)
+    println(toCsv)
 
 
 
