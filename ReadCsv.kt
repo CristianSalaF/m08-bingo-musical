048da7db290e402145fd9fe480c0d8f967802d35
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2024-12-19 08:57:08 +0100

    Add files via upload

---
+++ ReadCsv.kt
@@ -0,0 +1,10 @@
+import java.io.File
+fun readCsv() {
+    try {
+        val fileName = "bingo_musical.csv"
+        var lines:List<String> = File(fileName).readLines()
+        lines.forEach { line -> println(line) }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
