8860887150f93cefff8c2dd40e7326f9b13d18ab
vnaranjo <vnaranjo@bemen3.cat>
2024-12-21 07:51:41 +0100

    Refactor: extreure classe Music

---
+++ src/main/kotlin/Music.kt
@@ -0,0 +1,4 @@
+data class Music (
+    val trackId: Int,
+    val trackName: String,
+)
--- src/main/kotlin/ReadCsvLib.kt
+++ src/main/kotlin/ReadCsvLib.kt
@@ -1,13 +1,7 @@
-package org.example
 import java.nio.file.Paths
 import org.apache.commons.csv.*
 import java.io.BufferedReader
 import kotlin.io.path.reader
-
-data class Music (
-    val trackId: Int,
-    val trackName: String,
-)
 
 fun main() {
     val bufferedReader = BufferedReader(Paths.get("./bingo_musical.csv").reader());
