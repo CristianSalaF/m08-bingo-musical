3ddf7705babddff8869b0235b83208a3df42bd42
vnaranjo <vnaranjo@bemen3.cat>
2024-12-21 08:42:29 +0100

    Per qué funcionin els imports d'apache CSVParser (i qualsevol import), s'han d'afegir les llibreries al projecte (arxiu build.gradle).  Per això vam canviar a projecte Gradle. No oblideu fer file -> sync project with gradle files.  La majoria de les llibreries externes es poden trobar a maven.org

--- src/main/kotlin/ReadCsvLib.kt
+++ src/main/kotlin/ReadCsvLib.kt
@@ -1,9 +1,14 @@
+//ReadCsvLib
 import java.nio.file.Paths
-import org.apache.commons.csv.*
-import java.io.BufferedReader
 import kotlin.io.path.reader
+import java.io.BufferedReader
+import org.apache.commons.csv.CSVParser
+import org.apache.commons.csv.CSVFormat
+
 
 fun main() {
+    println("Hello too!")
+
     val bufferedReader = BufferedReader(Paths.get("./bingo_musical.csv").reader());
     val csvParser = CSVParser(bufferedReader, CSVFormat.DEFAULT);
     csvParser.forEach { csvLine -> println(csvLine) }
