f2c1ebe06b1b9074c1f502649dcb08714b5b7094
vnaranjo <vnaranjo@bemen3.cat>
2024-12-21 07:13:47 +0100

    Es poden omitir les claus, si no contenen res

--- src/main/kotlin/Carto.kt
+++ src/main/kotlin/Carto.kt
@@ -1,2 +1,1 @@
-class Carto(val fila1: ArrayList<Int>, val fila2: ArrayList<Int>, val fila3: ArrayList<Int>){
-}
+class Carto(val fila1: ArrayList<Int>, val fila2: ArrayList<Int>, val fila3: ArrayList<Int>)
