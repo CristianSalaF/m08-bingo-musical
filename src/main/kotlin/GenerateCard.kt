33bad860c557cd1d9ddad6a368723bc2fe805354
vnaranjo <vnaranjo@bemen3.cat>
2024-12-21 06:19:45 +0100

    Posar comentaris al codi

--- src/main/kotlin/GenerateCard.kt
+++ src/main/kotlin/GenerateCard.kt
@@ -3,36 +3,56 @@ import kotlin.random.Random
 
 
 fun main(){
+    //Columnes del cartró
     val columnes = 5
+    //Files del cartró
     val rows = 3
 
+    //Inicialització de variables
     var carton = Array(columnes) { Array<Int>(rows) { 0 } }
+    //nombres que ja s'han fet servir (Inicialment cap)
     val usedNums = mutableListOf(0)
 
+    //generació de nombres
+    //Nou número que es generarà
     var newnum:Int = 0;
+
+    //Recorregut per totes les files
     for (i in 0..rows-1){
+        //Recorregut per totes les columnes de cada fila
         for (j in 0..columnes-1){
+            //Si el número no s'ha generat ja, torna a generar un número
             while(usedNums.contains(newnum)){
-                newnum = Random.nextInt(0, 100)
+                newnum = Random.nextInt(1, 91)
             }
+            // Assignació del número generat prèviament
             carton[j][i]= newnum
+            // Generació d'un nou número aleatori
             usedNums.add(newnum)
+
+            //Per a cada columna es genera sempre un número aleatori extra  que no es farà servir
         }
     }
-    var toCsv: String ="";
-    for (i in 0..rows-1){
-        for (j in 0..columnes-1){
 
+    //Guardar el cartró en format csv
+    var toCsv: String ="";
+
+    //Recòrrer totes les files
+    for (i in 0..rows-1){
+        //Recòrrer totes les columnes
+        for (j in 0..columnes-1){
+            //Guardar cadascun dels números
             toCsv = toCsv + carton[j][i].toString()
+            //No posar la coma només al darrer element
             if(j != columnes-1){
                 toCsv = toCsv + ","
             }
         }
+        //Salt de línia quan
         toCsv= toCsv + "\n"
     }
+
+    //Imprimeix a pantalla per provar
     println(toCsv)
-
-
-
 }
 
